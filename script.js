5f71d01b29631638ae862d3500d2a9bfc74258b7
PDTDIV <153255420+PDTDIV@users.noreply.github.com>
2024-04-23 13:30:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const users = [
-    { username: 'SMR/PGT', password: '123' },
+    { username: 'smrpgt', password: 'pgt123' },
     { username: 'user2', password: 'password2' },
     { username: 'user3', password: 'password3' },
     { username: 'user4', password: 'password4' },
